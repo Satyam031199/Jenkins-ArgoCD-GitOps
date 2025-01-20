e804f63bd5d4d8afce0d8aedd7efcc9c8a265d30
Satyam Chaturvedi <83594305+Satyam031199@users.noreply.github.com>
2025-01-20 20:14:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  console.log('/hello is working');
+  console.log('/hello api is working');
   res.send('Hello, This is a Jenkins-ArgoCD-GitOps project');
 });
 
